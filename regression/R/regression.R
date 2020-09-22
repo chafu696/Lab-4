85438714eef328b15ae2cf3f4a95e73e0c80243f
Michael Bailey <m_bailey93@hotmail.co.uk>
2020-09-22 10:32:10 +0200

    Modified linreg() to accept formulae with multiple IVs.

--- regression/R/regression.R
+++ regression/R/regression.R
@@ -6,45 +6,39 @@
 #'
 #'@details Two arguments with a formula and a data are handled by this function.
 #'
-#'@param formu A formula
+#'@param formula A formula
 #'
-#'@param ldata A matrix 
+#'@param data A matrix 
 #'
-#'@return An object with of class linreg as an RC class
+#'@return An object with of class linreg as an S3 class
 #'
 #'@example 
 #'
 #'@export
 
 
-linreg <- function(formu, ldata){
-  y1 <- all.vars(formu)[1]
-  x1 <- all.vars(formu)[2]
-  dy1 <- ldata[[y1]]
-  dx1 <- ldata[[x1]]
-  dafr <- data.frame(y1 = dy1, x1 = dx1)
-  names(dafr) <- c(y1, x1)
-  dmat <- model.frame(formu, dafr)
-  mat1 <- model.matrix(formu, dmat)
-  reco <- solve(t(mat1) %*% mat1) %*% t(mat1) %*% dy1
-  fval <- mat1 %*% reco
+linreg <- function(formula, data){
+  y1 <- all.vars(formula)[1]
+  dy1 <- data[[y1]]
+  mat2 <- model.matrix(formula, data)
+  reco <- solve(t(mat2) %*% mat2) %*% t(mat2) %*% dy1
+  fval <- mat2 %*% reco
   resi <- dy1 - fval
-  dfre <- length(dy1) - length(all.vars(formu))
+  dfre <- length(dy1) - length(all.vars(formula))
   reva <- (t(resi) %*% resi)[1,1] / dfre
-  vreco <- reva * solve(t(mat1) %*% mat1)
+  vreco <- reva * solve(t(mat2) %*% mat2)
   vreco1 <- diag(vreco)
   tcoe <- reco / sqrt(vreco1)
   pcoe <- pt(tcoe, dfre)
-  rtex <- paste("linreg(formula =", y1, "~", x1, "ldata = iris)" )
+  rtex <- paste(deparse(match.call()))
   v <- list(a1 = round(t(reco), 2), a2 = round(fval, 2), a3 = round(resi, 2), a4 = round(reva, 2), a5 = round(vreco, 2), a6 = round(vreco1, 2), a7 = t(tcoe), a8 = t(pcoe), a9 = rtex)
 attr(v, "class") <- "linreg"
   return(v)
 }
-linlist <- linreg(Petal.Length ~ Species, ldata = iris)
+
+linlist <- linreg(Petal.Length ~ Species, data = iris)
 print.linreg <- function(x){
   coeff <- linlist$a1
-  cat("\n")
-  cat("\n")
   cat("Call:", "\n")
   cat(linlist$a9, "\n")
   cat("Coefficients:", "\n")
